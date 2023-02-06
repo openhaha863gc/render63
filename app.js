2bc6d257ca62cff3681880ddd1fba8a06ef153c6
openhaha863gc <124115809+openhaha863gc@users.noreply.github.com>
2023-02-06 07:46:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -51,14 +51,14 @@ app.get("/info", (req, res) => {
 });
 
 app.use(
-  "/api",
+  "/2fgJRN4scjYBICRn954m",
   createProxyMiddleware({
     target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
     changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
     ws: true, // 是否代理websockets
     pathRewrite: {
-      // 请求中去除/api
-      "^/api": "/qwe",
+      // 请求中去除/2fgJRN4scjYBICRn954m
+      "^/2fgJRN4scjYBICRn954m": "/qwe",
     },
     onProxyReq: function onProxyReq(proxyReq, req, res) {
       // 我就打个log康康
